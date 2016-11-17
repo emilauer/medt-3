813b9833094c3a93467f1040def959bb45d40e06
emilauer <emilauer7@gmail.com>
2016-11-17 14:48:28 +0100

    Simple Form

--- ue5/SimpleForm1.php
+++ ue5/SimpleForm1.php
@@ -38,80 +38,68 @@
 </head>
 <body>
 
-<div class="row">
+<div class="container anmeldekasterl">
 
-	<div class="col-md-3">
+	<h4>Anmeldedaten eingeben</h4>
 
-		<div class="container anmeldekasterl">
+	<form action="http://localhost/medt-3/ue5/SimpleForm1.php">
 
-			<h4>Anmeldedaten eingeben</h4>
+  		<div class="form-group">
+		    <label for="text">Vorname</label>
+		    <input type="name" class="form-control" name="vn">
+  		</div>
 
-			<form action="http://localhost/medt-3/ue5/SimpleForm1.php">
+  		<div class="form-group">
+    		<label for="text">Nachname</label>
+   			<input type="name" class="form-control" name="nn">
+  		</div>
 
-		  		<div class="form-group">
-				    <label for="text">Vorname</label>
-				    <input type="name" class="form-control" name="vn">
-		  		</div>
+  		<div class="form-group">
+    		<label for="email">Email</label>
+   			<input type="email" class="form-control" name="em">
+  		</div>
 
-		  		<div class="form-group">
-		    		<label for="text">Nachname</label>
-		   			<input type="name" class="form-control" name="nn">
-		  		</div>
+  		<div class="checkbox">
+    		<label><input type="checkbox"> Anmeldedaten merken</label>
+  		</div>
 
-		  		<div class="form-group">
-		    		<label for="email">Email</label>
-		   			<input type="email" class="form-control" name="em">
-		  		</div>
+ 		<button type="submit" class="btn btn-default" name="submitBtn" value="Anmelden">
+ 			Anmelden
+		</button>
 
-		  		<div class="checkbox">
-		    		<label><input type="checkbox"> Anmeldedaten merken</label>
-		  		</div>
-
-		 		<button type="submit" class="btn btn-default" name="submitBtn" value="Anmelden">
-		 			Anmelden
-				</button>
-
-			</form>
-
-		</div>
-
-	</div>
-
-	<div class="col-md-3">
-
-		<div class="container anmeldeinfo">
-
-		<div class="panel panel-primary">
-		 	<div class="panel-heading">
-				<h3 class="panel-title">Anmeldeinformationen</h3>
-		  	</div>
-		  	<div class="panel-body">
-		    	<?php
-	    			echo "<p><strong>Vorname</strong>";
-	    			if (isset($_GET['submitBtn'])) {
-						echo "<br>".$_GET['vn'];
-					}
-					echo "<p><strong>Nachname</strong>";
-	    			if (isset($_GET['submitBtn'])) {
-						echo "<br>".$_GET['nn'];
-					}
-					echo "<p><strong>Email</strong>";
-	    			if (isset($_GET['submitBtn'])) {
-						echo "<br>".$_GET['em'];
-		    		}
-						
-				?>
-		  		<!--<button class="btn btn-primary" type="submit">Bestätigen</button>-->
-		  	</div>
-		</div>
-
-		</div>
-
-	</div>
-
+	</form>
 
 </div>
 
+	
+
+<div class="container anmeldeinfo">
+
+<div class="panel panel-primary">
+ 	<div class="panel-heading">
+		<h3 class="panel-title">Anmeldeinformationen</h3>
+  	</div>
+  	<div class="panel-body">
+    	<?php
+			echo "<p><strong>Vorname</strong>";
+			if (isset($_GET['submitBtn'])) {
+				echo "<br>".$_GET['vn'];
+			}
+			echo "<p><strong>Nachname</strong>";
+			if (isset($_GET['submitBtn'])) {
+				echo "<br>".$_GET['nn'];
+			}
+			echo "<p><strong>Email</strong>";
+			if (isset($_GET['submitBtn'])) {
+				echo "<br>".$_GET['em'];
+    		}
+				
+		?>
+  		<!--<button class="btn btn-primary" type="submit">Bestätigen</button>-->
+  	</div>
+</div>
+
+</div>
 
 
 </body>
